87e2db868cbd0f9e37ad99020985dd4f6b4d0807
aiturbidemil <aiturbide@milaboratories.com>
2024-09-30 12:35:05 +0200

    mid migration 2

--- pl-table/src/components/PlTable/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as PlTable } from "./PlTable.vue";
-
-export * from "./types";
--- pl-table/src/components/types.ts
+++
@@ -1,8 +0,0 @@
-import { Platforma } from "@platforma-sdk/model";
-
-declare global {
-    const platforma: Platforma | undefined;
-    interface Window {
-      platforma: Platforma | undefined;
-    }
-  }
