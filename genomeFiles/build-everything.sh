b156997ee6d713388ee9afbf40f4096cbb21c960
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2024-11-10 14:31:21 +0100

    fix

--- genomeFiles/build-everything.sh
+++ genomeFiles/build-everything.sh
@@ -5,8 +5,7 @@ set -o nounset
 
 JSON_FILE="${1}"
 
-# jq -r 'keys[]' "${JSON_FILE}" | 
-echo "Saccharomyces_cerevisiae" |
+jq -r 'keys[]' "${JSON_FILE}" | 
     while read -r species; do
         ./run_star_index.sh "${species}" ./genomeFileUrls.json "./indexed_genome/${species}"
     done
