157525a35e1f99b5090a508cd0dc771b80d00b48
aiturbidemil <aiturbide@milaboratories.com>
2024-11-11 10:07:23 +0100

    sample qc set up

--- ui/src/app.ts
+++ ui/src/app.ts
@@ -2,17 +2,13 @@ import { model } from '@platforma-open/milaboratories.star-read-mapping.model';
 import { defineApp } from '@platforma-sdk/ui-vue';
 import PrincipalComponentAnalysis from './pages/PCAPage.vue';
 import Settings from './pages/MainPage.vue';
-import SequenceDataQC from './pages/QCPage.vue';
-import HierarchicalClustering from './pages/HCPage.vue';
 
 
 export const sdkPlugin = defineApp(model, () => {
   return {
     routes: {
       '/': Settings,
-      '/QC': SequenceDataQC,
-      '/PCA': PrincipalComponentAnalysis,
-      '/HC': HierarchicalClustering
+      '/PCA': PrincipalComponentAnalysis
     }
   };
 });
--- ui/src/pages/PCAPage.vue
+++ ui/src/pages/PCAPage.vue
@@ -1,34 +1,34 @@
 <script setup lang="ts">
-import { PlTextArea } from '@milaboratories/uikit';
-import { ref } from 'vue';
-import { GraphMaker } from "@milaboratories/graph-maker";
-import { GraphMakerSettings } from "@milaboratories/graph-maker/dist/GraphMaker/types";
-import { model } from "@platforma-open/milaboratories.star-read-mapping.model";
+// import { ref } from 'vue';
+import { GraphMakerSettings, GraphMaker } from '@milaboratories/graph-maker';
+//import { model } from "@platforma-open/milaboratories.star-read-mapping.model";
 import "@milaboratories/graph-maker/dist/style.css";
 import { useApp } from "../app";
 
 const app = useApp();
 
-const settings = ref({
-  chartType: "discrete",
-  template: null,
-  optionsState: null,
-  statisticsSettings: null,
-  axesSettings: null,
-  layersSettings: null,
-  dataBindAes: null,
-} satisfies GraphMakerSettings);
+const settings = {
+  chartType: 'scatterplot',
+  template: 'dots',
+  title: '',
+  defaultOptions: [{
+    inputName: 'y',
+    selectedSource: {
+      kind: 'PColumn',
+      name: 'countMatrix',
+      valueType: 'Int'
+    }
+  }]
+   } as GraphMakerSettings;
 </script>
 
 <template>
-  <GraphMaker
-    v-if="app.outputs.pf?.ok && app.outputs.pf.value"
-    :p-frame-handle="app.outputs.pf.value"
-    :settings="settings"
-    :p-frame-driver="model.pFrameDriver"
-    graph-title="Title"
-  />
-  <div v-else>"Nothing"</div>
+  <div class="container_graph_page" :key="app.queryParams.id">
+    <graph-maker
+      v-model="settings"
+      :pFrame="app.model.outputs.pf"
+    />
+  </div>
 </template>
 
 
