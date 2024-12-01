75f972fcb5f07b9c88c7120eb4459ec0a4ae1f1f
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2024-12-01 18:35:21 +0100

    wip

--- ui/src/pages/FeatureCountsStatsCell.vue
+++ ui/src/pages/FeatureCountsStatsCell.vue
@@ -4,10 +4,56 @@ import FeatureCountsRow from './components/FeatureCountsRow.vue';
 import { FeatureCountsQC } from './results';
 
 defineProps<{
-    params: ICellRendererParams<{featureCountsQc: FeatureCountsQC}>;
+    params: ICellRendererParams<{ featureCountsQc: FeatureCountsQC }>;
 }>();
 </script>
 
 <template>
-    <FeatureCountsRow style="height: 100%;" :align-report="params.data?.featureCountsQc" />
+    <div v-if="!params.data?.featureCountsQc" :class="{ 'spinner-cell__white-bg': true }" class="spinner-cell">
+        <div class="spinner-cell__indicator" :style="{ width: '100%' }"></div>
+    </div>
+    <FeatureCountsRow v-else style="height: 100%;" :align-report="params.data?.featureCountsQc" />
 </template>
+
+<style lang="css">
+.spinner-cell {
+    background-color: transparent;
+    height: 100%;
+    position: relative;
+    width: 100%;
+    overflow: hidden;
+    border-radius: 2px;
+    /* border-left: 1px solid var(--border-color-div-grey);
+    border-right: 1px solid var(--border-color-div-grey); */
+}
+
+.spinner-cell__white-bg {
+    background-color: #fff;
+}
+
+.spinner-cell__indicator {
+    position: absolute;
+    height: 100%;
+    transition: width 0.4s ease-in-out;
+    /* background: linear-gradient(90deg, #FFF 0%, #D8FAC8 100%); */
+    --c: no-repeat linear-gradient(90deg, #FFF 0%, #D8FAC8 100%);
+    background: var(--c), var(--c), #FFF;
+    background-size: 60% 100%;
+
+    animation: l16 5s infinite;
+}
+
+@keyframes l16 {
+    0% {
+        background-position: -150% 0, -150% 0
+    }
+
+    66% {
+        background-position: 250% 0, -150% 0
+    }
+
+    100% {
+        background-position: 250% 0, 250% 0
+    }
+}
+</style>
--- ui/src/pages/ReportPanel.vue
+++ ui/src/pages/ReportPanel.vue
@@ -1,20 +1,36 @@
 <script setup lang="ts">
 import { PlBtnGroup, PlLogView } from '@platforma-sdk/ui-vue';
 import { computed, ref } from 'vue';
+import AlignmentRow from './components/AlignmentRow.vue';
+import FeatureCountsRow from './components/FeatureCountsRow.vue';
 import { resultMap } from './results';
 
 
 const sampleId = defineModel<string | undefined>()
 
 const starProgress = computed(() => {
-    
     if (sampleId.value === undefined || resultMap.value === undefined)
         return undefined;
     else {
-        console.log(resultMap.value[sampleId.value].starProgress);
         return resultMap.value[sampleId.value].starProgress;
     }
 });
+
+const starQc = computed(() => {
+    if (sampleId.value === undefined || resultMap.value === undefined)
+        return undefined;
+    else {
+        return resultMap.value[sampleId.value].starQC;
+    }
+})
+
+const featureCountsQc = computed(() => {
+    if (sampleId.value === undefined || resultMap.value === undefined)
+        return undefined;
+    else {
+        return resultMap.value[sampleId.value].featureCountsQC;
+    }
+})
 
 const featureCountsProgress = computed(() => {
     if (sampleId.value === undefined || resultMap.value === undefined)
@@ -24,7 +40,19 @@ const featureCountsProgress = computed(() => {
     }
 });
 
+
 const options = [{
+    label: 'Quality Controls',
+    value: 'qc'
+},
+{
+    label: 'Analysis Logs',
+    value: 'logs'
+}]
+
+const currentView = ref('qc')
+
+const logOptions = [{
     label: 'Mapping log',
     value: 'star'
 },
@@ -33,12 +61,22 @@ const options = [{
     value: 'subread'
 }]
 
-const currentView = ref('star')
+const currentLogView = ref('star')
 
 </script>
 
 <template>
     <PlBtnGroup v-model="currentView" :options="options" />
-    <PlLogView v-if="currentView === 'star'" :logHandle="starProgress" />
-    <PlLogView v-if="currentView === 'subread'" :logHandle="featureCountsProgress" />
+    <template v-if="currentView === 'qc'">
+        <h3>Mapping</h3>
+        <AlignmentRow :alignReport="starQc" size="large" showFractionInLabel />
+        <h3>Feature assignment</h3>
+        <FeatureCountsRow :alignReport="featureCountsQc" size="large" showFractionInLabel />
+    </template>
+
+    <template v-if="currentView === 'logs'">
+        <PlBtnGroup v-model="currentLogView" :options="logOptions" />
+        <PlLogView v-if="currentLogView === 'star'" :logHandle="starProgress" />
+        <PlLogView v-if="currentLogView === 'subread'" :logHandle="featureCountsProgress" />
+    </template>
 </template>
