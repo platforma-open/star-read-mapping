b12cf0ec147d258feeca55a9487a5a6a9e9b8b3f
Vladimir Antropov <vladimir.antropov@milaboratories.com>
2024-12-02 13:11:17 +0100

    chore: fix script path

--- genomeFiles/build-everything.sh
+++ genomeFiles/build-everything.sh
@@ -8,5 +8,5 @@ JSON_FILE="${1}"
 jq -r 'keys[]' "${JSON_FILE}" | 
     while read -r species; do
          export PARENT_SCRIPT_PATH=$(dirname "$(realpath "$0")")
-        ./run_star_index.sh "${species}" ./genomeFileLocal.json "./indexed_genome/${species}"
+        ./run_star_index.sh "${species}" ./genomeFileUrls_All.json "./indexed_genome/${species}"
     done
