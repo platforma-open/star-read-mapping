9302b087270dc6cfb988f9fbe8bcf75199156e18
nikolaygiman <gimanhead@gmail.com>
2024-12-24 13:53:19 +0100

    fix: updated PlAgCellProgress

--- ui/src/pages/FeatureCountsStatsCell.vue
+++ ui/src/pages/FeatureCountsStatsCell.vue
@@ -2,58 +2,17 @@
 import { ICellRendererParams } from 'ag-grid-enterprise';
 import FeatureCountsRow from './components/FeatureCountsRow.vue';
 import { FeatureCountsQC } from './results';
+import { PlAgCellProgress } from '@platforma-sdk/ui-vue';
 
 defineProps<{
-    params: ICellRendererParams<{ featureCountsQc: FeatureCountsQC }>;
+  params: ICellRendererParams<{ featureCountsQc: FeatureCountsQC }>;
 }>();
 </script>
 
 <template>
-    <div v-if="!params.data?.featureCountsQc" :class="{ 'spinner-cell__white-bg': true }" class="spinner-cell">
-        <div class="spinner-cell__indicator" :style="{ width: '100%' }"></div>
-    </div>
-    <FeatureCountsRow v-else style="height: 100%;" :align-report="params.data?.featureCountsQc" />
+  <PlAgCellProgress
+    v-if="!params.data?.featureCountsQc"
+    v-bind="{ params: { ...params, stage: 'running', step: '', progressString: '' } }"
+  />
+  <FeatureCountsRow v-else style="height: 100%" :align-report="params.data?.featureCountsQc" />
 </template>
-
-<style lang="css">
-.spinner-cell {
-    background-color: transparent;
-    height: 100%;
-    position: relative;
-    width: 100%;
-    overflow: hidden;
-    border-radius: 2px;
-    /* border-left: 1px solid var(--border-color-div-grey);
-    border-right: 1px solid var(--border-color-div-grey); */
-}
-
-.spinner-cell__white-bg {
-    background-color: #fff;
-}
-
-.spinner-cell__indicator {
-    position: absolute;
-    height: 100%;
-    transition: width 0.4s ease-in-out;
-    /* background: linear-gradient(90deg, #FFF 0%, #D8FAC8 100%); */
-    --c: no-repeat linear-gradient(90deg, #FFF 0%, #D8FAC8 100%);
-    background: var(--c), var(--c), #FFF;
-    background-size: 60% 100%;
-
-    animation: l16 5s infinite;
-}
-
-@keyframes l16 {
-    0% {
-        background-position: -150% 0, -150% 0
-    }
-
-    66% {
-        background-position: 250% 0, -150% 0
-    }
-
-    100% {
-        background-position: 250% 0, 250% 0
-    }
-}
-</style>
--- ui/src/pages/components/ProgressCell.vue
+++ ui/src/pages/components/ProgressCell.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 import { ICellRendererParams } from 'ag-grid-enterprise';
 import { computed, unref } from 'vue';
+import { PlAgCellProgress, PlProgressCellProps } from '@platforma-sdk/ui-vue';
 
 const props = defineProps<{
-  params: ICellRendererParams
+  params: ICellRendererParams;
 }>();
 
 const progressString = computed(() => {
@@ -30,11 +31,11 @@ const parsed = computed<Parsed>(() => {
   }
 
   console.log(raw);
-  if (raw.indexOf(".....") < 0) {
+  if (raw.indexOf('.....') < 0) {
     return res;
   }
 
-  const parts = raw.split(".....");
+  const parts = raw.split('.....');
 
   res.time = parts[0].trim();
   res.stage = parts[1].trim();
@@ -44,13 +45,13 @@ const parsed = computed<Parsed>(() => {
       res.percentage = '0';
       break;
     case 'started mapping':
-      res.percentage = '33'
+      res.percentage = '33';
       break;
     case 'started sorting BAM':
-      res.percentage = '66'
+      res.percentage = '66';
       break;
     case 'finished successfully':
-      res.percentage = '100'
+      res.percentage = '100';
       break;
   }
 
@@ -61,71 +62,16 @@ const parsed = computed<Parsed>(() => {
   return res;
 });
 
-const canShowBackground = computed(
-  () => parsed.value.stage !== 'Queued'
-);
+const ProgressProps = computed<PlProgressCellProps>(() => {
+  return {
+    stage: parsed.value.stage === 'Queued' ? 'not_started' : 'running',
+    step: parsed.value.stage || '',
+    progress: parsed.value.percentage ? +parsed.value.percentage : 0,
+    progressString: parsed.value.percentageLabel || ''
+  };
+});
 </script>
 
 <template>
-  <div :class="{ 'progress-cell__white-bg': canShowBackground }" class="progress-cell">
-    <div class="progress-cell__indicator" :style="{ width: parsed.percentage + '%' }"></div>
-    <div class="progress-cell__body">
-      <div :class="{ 'progress-cell__stage--queued': parsed.stage === 'Queued' }" class="progress-cell__stage">
-        {{ parsed.stage }}
-      </div>
-      <div class="progress-cell__percentage">{{ parsed.percentageLabel }}</div>
-    </div>
-  </div>
+  <PlAgCellProgress v-bind="{ params: { ...props.params, ...ProgressProps } }" />
 </template>
-
-<style lang="css">
-.progress-cell {
-  background-color: transparent;
-  height: 100%;
-  position: relative;
-  width: 100%;
-  overflow: hidden;
-  border-radius: 2px;
-  /* border-left: 1px solid var(--border-color-div-grey);
-    border-right: 1px solid var(--border-color-div-grey); */
-}
-
-.progress-cell__white-bg {
-  background-color: #fff;
-}
-
-.progress-cell__indicator {
-  position: absolute;
-  height: 100%;
-  transition: width 0.4s ease-in-out;
-  /* background: linear-gradient(90deg, #FFF 0%, #D8FAC8 100%); */
-  background: linear-gradient(90deg, #fff 0%, #d8fac8 100%);
-}
-
-.progress-cell__body {
-  padding: 0 15px;
-  display: flex;
-  gap: 12px;
-  align-items: center;
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  z-index: 1;
-}
-
-.progress-cell__stage {
-  overflow: hidden;
-  text-overflow: ellipsis;
-  flex-shrink: 1;
-}
-
-.progress-cell__percentage {
-  flex-grow: 1;
-  flex-shrink: 0;
-  text-align: right;
-}
-
-.progress-cell__stage--queued {
-  color: var(--txt-03);
-}
-</style>
