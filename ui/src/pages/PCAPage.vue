e77ac5f3dda51163acff0f420cc824eab4f54bb8
Julen Mendieta <jmendieta@milaboratories.com>
2025-09-15 13:25:22 +0200

    Full SDK update

--- ui/src/pages/PCAPage.vue
+++ ui/src/pages/PCAPage.vue
@@ -3,40 +3,34 @@ import '@milaboratories/graph-maker/styles';
 // import { ref } from 'vue';
 import type { PredefinedGraphOption } from '@milaboratories/graph-maker';
 import { GraphMaker } from '@milaboratories/graph-maker';
+import type { PColumnIdAndSpec } from '@platforma-sdk/model';
+import { computed } from 'vue';
 import { useApp } from '../app';
 
 const app = useApp();
 
-const defaultOptions: PredefinedGraphOption<'scatterplot'>[] = [
-  {
-    inputName: 'x',
-    selectedSource: {
-      kind: 'PColumn',
-      name: 'pl7.app/rna-seq/pc1',
-      valueType: 'Double',
-      axesSpec: [
-        {
-          name: 'pl7.app/sampleId',
-          type: 'String',
-        },
-      ],
+const defaultOptions = computed((): PredefinedGraphOption<'scatterplot'>[] | undefined => {
+  if (!app.model.outputs.pcaPcols)
+    return undefined;
+
+  const pcaPcols = app.model.outputs.pcaPcols;
+  function getIndex(name: string, pcols: PColumnIdAndSpec[]): number {
+    return pcols.findIndex((p) => (p.spec.name === name));
+  }
+
+  const defaults: PredefinedGraphOption<'scatterplot'>[] = [
+    {
+      inputName: 'x',
+      selectedSource: pcaPcols[getIndex('pl7.app/rna-seq/pc1', pcaPcols)].spec,
     },
-  },
-  {
-    inputName: 'y',
-    selectedSource: {
-      kind: 'PColumn',
-      name: 'pl7.app/rna-seq/pc2',
-      valueType: 'Double',
-      axesSpec: [
-        {
-          name: 'pl7.app/sampleId',
-          type: 'String',
-        },
-      ],
+    {
+      inputName: 'y',
+      selectedSource: pcaPcols[getIndex('pl7.app/rna-seq/pc2', pcaPcols)].spec,
     },
-  },
-];
+  ];
+  return defaults;
+});
+
 </script>
 
 <template>
